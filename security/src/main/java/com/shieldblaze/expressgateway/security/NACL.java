6b16b1406f17cdf1b2e07da96dcf32db6d99599e
Aayush Atharva <aayush@shieldblaze.com>
2020-10-14 19:42:02 +0530

    Create Security Module

--- security/src/main/java/com/shieldblaze/expressgateway/security/NACL.java
+++ security/src/main/java/com/shieldblaze/expressgateway/security/NACL.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with ShieldBlaze ExpressGateway.  If not, see <https://www.gnu.org/licenses/>.
  */
-package com.shieldblaze.expressgateway.core.security;
+package com.shieldblaze.expressgateway.security;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
