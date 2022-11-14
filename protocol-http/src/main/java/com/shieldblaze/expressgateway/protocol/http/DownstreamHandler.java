c81c69cc84cf9e5ed7c0b5e07884482df25afe95
Aayush Atharva <aayush@shieldblaze.com>
2022-11-14 22:58:49 +0530

    Correctly handle GOAWAY

--- protocol-http/src/main/java/com/shieldblaze/expressgateway/protocol/http/DownstreamHandler.java
+++ protocol-http/src/main/java/com/shieldblaze/expressgateway/protocol/http/DownstreamHandler.java
@@ -110,28 +110,23 @@ final class DownstreamHandler extends ChannelInboundHandlerAdapter implements Cl
             }
         } else if (msg instanceof Http2GoAwayFrame goAwayFrame) {
             if (isConnectionHttp2) {
-                // If LastStreamID is 'Integer.MAX_VALUE' then we have to shut down the entire connection.
-                if (goAwayFrame.lastStreamId() == Integer.MAX_VALUE) {
-                    inboundChannel.writeAndFlush(msg);
-                } else {
-                    Http2GoAwayFrame http2GoAwayFrame = new DefaultHttp2GoAwayFrame(goAwayFrame.errorCode(), goAwayFrame.content());
+                Http2GoAwayFrame http2GoAwayFrame = new DefaultHttp2GoAwayFrame(goAwayFrame.errorCode(), goAwayFrame.content());
 
-                    if (goAwayFrame.lastStreamId() == Integer.MAX_VALUE) {
+                if (goAwayFrame.lastStreamId() == Integer.MAX_VALUE) {
+                    http2GoAwayFrame.setExtraStreamIds(Integer.MAX_VALUE);
+                } else {
+                    StreamPropertyMap.StreamProperty streamProperty = httpConnection.streamPropertyMap().get(goAwayFrame.lastStreamId());
+
+                    if (streamProperty == null) {
                         http2GoAwayFrame.setExtraStreamIds(Integer.MAX_VALUE);
                     } else {
-                        StreamPropertyMap.StreamProperty streamProperty = httpConnection.streamPropertyMap().get(goAwayFrame.lastStreamId());
-
-                        if (streamProperty == null) {
-                            http2GoAwayFrame.setExtraStreamIds(Integer.MAX_VALUE);
-                        } else {
-                            int clientId = streamProperty.clientFrameStream().id();
-                            http2GoAwayFrame.setExtraStreamIds(clientId);
-                            httpConnection.streamPropertyMap().remove(goAwayFrame.lastStreamId());
-                        }
+                        int clientId = streamProperty.clientFrameStream().id();
+                        http2GoAwayFrame.setExtraStreamIds(clientId);
+                        httpConnection.streamPropertyMap().remove(goAwayFrame.lastStreamId());
                     }
-
-                    inboundChannel.writeAndFlush(http2GoAwayFrame);
                 }
+
+                inboundChannel.writeAndFlush(http2GoAwayFrame);
             }
         } else if (msg instanceof Http2ResetFrame http2ResetFrame) {
             if (isConnectionHttp2) {
